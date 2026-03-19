265249da67064bcd5e28e31078c7e2b8cd9c8991
retiq <idzkowskit@gmail.com>
2026-03-19 12:59:35 +0100

    expanded on the work

--- src/colorizer.py
+++ src/colorizer.py
@@ -1,3 +1,15 @@
 # TODO:
 # Implement the colorizer
 # For now it shall return an HTML with colors from the languages color_scheme
+from dataclasses import dataclass
+
+from src.language import ColorScheme
+from src.tokens import Token
+
+
+@dataclass(slots=True, frozen=True)
+class Colorizer[ET]:
+    color_scheme: ColorScheme
+
+    def colorize(self, tokens: list[Token[ET]]) -> str:
+        return ""
